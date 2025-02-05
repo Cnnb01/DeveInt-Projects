5fdfc46a53751c82041b69bbe9b239ca46a0ab1c
Cnnb01 <nyamambanyaguch@gmail.com>
2025-02-05 07:28:54 +0300

    An frame selling e-commerce site

--- E-COMMERCE/server.js
+++ E-COMMERCE/server.js
@@ -1,11 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
-import Client from "pg";
-// import pg from "pg";
+// import Client from "pg";
+import pg from "pg";
 import axios from "axios";
 
 const app = express()
 const port = 3000
+const db = new pg.Client({
+    user:"postgres",
+    host:"localhost",
+    database:"world",
+    password:"postgres1",
+    port: 5432
+});
+db.connect();
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended:true}));
